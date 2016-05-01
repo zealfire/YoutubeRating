732a6352de4d63ceecd0e4f6d2ba16d5ad80bec2
zealfire <iamalok185@gmail.com>
2016-05-02 02:01:39 +0530

    logic for mutation observer added.

--- js/content.js
+++ js/content.js
@@ -1,18 +1,21 @@
-function threads() {
-    var data = $(document).find(".like-button-renderer-like-button-unclicked").text();
-    var p = $('.stat.view-count').parent();
+function threads(flag="") {
+    if(flag != "") {
+        flag = "#" + flag + " "; 
+    }
+    var resolver = flag + '.stat.view-count';
+    var p = $(resolver).parent();
     var ajaxRequests = []
-    for(var i=0;i < p.length; i++) {
+    for(var i=0;i < 2; i++) {
         ajaxRequests.push(jQuery.get(p[i].href));
     }
-    var p1 = document.getElementsByClassName('stat view-count');
+    var p1 = $(resolver);
     for(var i=0; i < p1.length; i++) {
         var childGuest = document.createElement("span");
         childGuest.className = "yt-spinner-img  yt-sprite";
         p1[i].parentNode.insertBefore(childGuest, p1[i].nextSibling);
     }
     jQuery.when.apply(jQuery, ajaxRequests).done(function () {
-        for (var i=0; i < p.length; i++) {
+        for (var i=0; i < 2; i++) {
             result = $(arguments[i][0]).find(".like-button-renderer-like-button-unclicked").text();
             var childGuest = document.createElement("span");
             childGuest.className = "stat";
@@ -22,7 +25,22 @@ function threads() {
         }
     });
 }
+
+function dom_changes_recorder() {
+    var target = document.getElementById('watch-more-related');
+    var observer = new MutationObserver(function(mutations) {  
+      mutations.forEach(function(mutation) {
+        if(mutation.type == 'childList') {
+            threads("watch-more-related");
+        }
+      });    
+    });
+    var config = { attributes: true, childList: true, characterData: true };
+    observer.observe(target, config);
+}
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     var curr = request.data.url
+    dom_changes_recorder();
     threads();
 });
