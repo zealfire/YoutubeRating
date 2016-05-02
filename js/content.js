6e3df643961109222ba51b850927fa2422af5bd0
zealfire <iamalok185@gmail.com>
2016-05-02 21:05:14 +0530

    name of extension changed.

--- js/content.js
+++ js/content.js
@@ -5,7 +5,7 @@ function threads(flag="") {
     var resolver = flag + '.stat.view-count';
     var p = $(resolver).parent();
     var ajaxRequests = []
-    for(var i=0;i < 2; i++) {
+    for(var i=0;i < p.length; i++) {
         ajaxRequests.push(jQuery.get(p[i].href));
     }
     var p1 = $(resolver);
@@ -15,11 +15,13 @@ function threads(flag="") {
         p1[i].parentNode.insertBefore(childGuest, p1[i].nextSibling);
     }
     jQuery.when.apply(jQuery, ajaxRequests).done(function () {
-        for (var i=0; i < 2; i++) {
+        for (var i=0; i < p1.length; i++) {
             result = $(arguments[i][0]).find(".like-button-renderer-like-button-unclicked").text();
             var childGuest = document.createElement("span");
-            childGuest.className = "stat";
+            childGuest.className = "stat like-button-renderer-like-button yt-uix-button-has-icon no-icon-markup yt-uix-button yt-uix-button-opacity";
             childGuest.innerHTML = result;
+            childGuest.style.marginLeft = "-4%";
+            childGuest.style.marginRight = "-4%";
             p1[i].parentNode.insertBefore(childGuest, p1[i].nextSibling);
             $(childGuest).next().remove();
         }
